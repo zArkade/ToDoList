6afaa5afada35906b77d8af31dad223db5397fd5
zArkade <fuz.ribeiro@hotmail.com>
2024-07-01 17:26:28 -0300

    Ajustes em geral, como json, espaçamentos, pode ignorar.

--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from datetime import datetime
 from tkinter import messagebox
 from tkinter import simpledialog
-from gerenciadorTask import add_task, edit_task, list_tasks, mark_task_completed, remove_task, save_tasks, load_tasks, search_tasks, tasks
+from gerenciadorTask import add_task, edit_task_completed, list_tasks, mark_task_completed, remove_task_completed, save_tasks, load_tasks, search_tasks, tasks
 
 class ToDoListApp:
     def __init__(self, root):
@@ -12,48 +12,48 @@ class ToDoListApp:
         self.unsaved_changes = False
 
         self.frame = tk.Frame(root)
-        self.frame.pack(padx=10, pady=10)
+        self.frame.pack(padx = 10, pady = 10)
 
         self.task_entry = tk.Entry(self.frame, width = 40)
-        self.task_entry.pack( side = tk.LEFT, padx = (0,10))
+        self.task_entry.pack(side = tk.LEFT, padx = (0, 10))
 
         self.due_date_var = tk.StringVar()
-        self.due_date_entry = tk.Entry(self.frame, width=10, textvariable=self.due_date_var)
-        self.due_date_entry.pack(side=tk.LEFT, padx=(0, 10))
+        self.due_date_entry = tk.Entry(self.frame, width = 10, textvariable = self.due_date_var)
+        self.due_date_entry.pack(side = tk.LEFT, padx = (0, 10))
         self.due_date_var.set("__/__/__")
         self.due_date_entry.bind("<KeyRelease>", self.format_date)
 
         self.add_button = tk.Button(self.frame, text = "Adicionar uma Tarefa", command = self.add_task)
-        self.add_button.pack( side = tk.LEFT)
+        self.add_button.pack(side = tk.LEFT)
 
-        self.task_listbox = tk.Listbox( root, width = 50, height = 15)
-        self.task_listbox.pack( padx = 10, pady = (10, 0))
+        self.task_listbox = tk.Listbox(root, width = 70, height = 15)
+        self.task_listbox.pack(padx = 10, pady = (10, 0))
 
         self.mark_button = tk.Button(root, text = "Marcar como concluída", command = self.mark_task)
-        self.mark_button.pack( side = tk.LEFT, padx = 10, pady = (10, 10))
+        self.mark_button.pack(side = tk.LEFT, padx=10, pady=(10, 10))
 
         self.remove_button = tk.Button(root, text = "Remover Tarefa", command = self.remove_task)
-        self.remove_button.pack( side = tk.LEFT, padx = 10, pady = (10, 10))
+        self.remove_button.pack(side = tk.LEFT, padx = 10, pady = (10, 10))
 
-        self.save_button = tk.Button(root, text = "Salvar Tarefas", command = save_tasks)
-        self.save_button.pack( side = tk.LEFT, padx = 10, pady = (10, 10))
+        self.save_button = tk.Button(root, text = "Salvar Tarefas", command = self.save_tasks)
+        self.save_button.pack(side = tk.LEFT, padx = 10, pady = (10, 10))
 
         self.load_button = tk.Button(root, text = "Carregar Tarefas", command = self.load_tasks)
-        self.load_button.pack( side = tk.LEFT, padx = 10, pady = (10, 10))
+        self.load_button.pack(side = tk.LEFT, padx = 10, pady = (10, 10))
 
-        self.edit_button = tk.Button(root, text="Editar Tarefa", command=self.edit_task)
-        self.edit_button.pack(side=tk.LEFT, padx=10, pady=(10, 10))
+        self.edit_button = tk.Button(root, text = "Editar Tarefa", command = self.edit_task_completed)
+        self.edit_button.pack(side = tk.LEFT, padx = 10, pady = (10, 10))
 
-        self.search_entry = tk.Entry(root, width=40)
-        self.search_entry.pack(padx=10, pady=(10, 0))
+        self.search_entry = tk.Entry(root, width = 40)
+        self.search_entry.pack(padx = 10, pady = (10, 0))
 
-        self.search_button = tk.Button(root, text="Pesquisar", command=self.search_task)
-        self.search_button.pack(padx=10, pady=(0, 10))
+        self.search_button = tk.Button(root, text = "Pesquisar", command = self.search_task)
+        self.search_button.pack(padx = 10, pady = (0, 10))
 
-        self.filter_var = tk.StringVar(value="Todas")
+        self.filter_var = tk.StringVar(value = "Todas")
 
-        self.filter_menu = tk.OptionMenu(root, self.filter_var, "Todas", "Concluídas", "Pendentes", command=self.filter_tasks)
-        self.filter_menu.pack(padx=10, pady=(10, 10))
+        self.filter_menu = tk.OptionMenu(root, self.filter_var, "Todas", "Concluídas", "Pendentes", command = self.filter_tasks)
+        self.filter_menu.pack(padx = 10, pady = (10, 10))
 
         self.load_tasks()
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
@@ -136,13 +136,6 @@ class ToDoListApp:
             task_index = self.task_listbox.curselection()[0]
             new_description = simpledialog.askstring("Editar Tarefa", "Nova descrição da tarefa:")
             if new_description:
-                try:
-                    if new_due_date:
-                        due_date_obj = datetime.datetime.strptime(new_due_date, "%d/%m/%Y")
-                        if due_date_obj.date() <= datetime.datetime.today().date():
-                            messagebox.showwarning("Data Inválida", "A data deve ser posterior ao dia de hoje.")
-                            return
-                    edit_task(task_index, new_description, new_due_date)
                 self.unsaved_changes = True
                 edit_task_completed(task_index, new_description)
                 #     self.update_task_list()
